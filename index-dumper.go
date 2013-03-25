e223bdeb4903b0756ed4f1ab7070a591f44ff5da
Diego Waxemberg <diego.waxemberg@gmail.com>
2013-03-25 10:44:52 -0400

    created an index-dumper; it is still unfinished

---
+++ index-dumper.go
@@ -0,0 +1,13 @@
+package main 
+
+import "io/ioutil"
+
+func main() {
+}
+
+func dump(ip string, data string) {
+    err := ioutil.WriteFile(ip, []byte(data), 0660 )
+    if err != nil {
+        panic(err)
+    }
+}
