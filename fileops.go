ee608c9e6be5bf051d7873ce8717745b547251b8
Brendan Higgins <bjh83@case.edu>
2013-03-26 04:13:06 -0400

    I added some of the client code

--- fileops.go
+++ fileops.go
@@ -2,6 +2,9 @@ package main
 
 import(
 	"path"
+	"net"
+	"crypto/md5"
+	"strconv"
 )
 
 const SandwichFolderName = "Sandwich"
@@ -12,4 +15,24 @@ func ConfPath(newPath string) string {
 	return path.Join("conf", newPath)
 }
 
+func ComputeHash(address net.IP) int {
+	var port uint16
+	hash := md5.New()
+	hash.Write([]byte(address))
+	result := hash.Sum(nil)
+	port = (uint16(result[0]) + uint16(result[3])) << 8
+	port += uint16(result[1]) + uint16(result[2])
+	if port < 1024 {
+		port += 1024
+	}
+	return int(port)
+}
+
+func GetPort(address net.IP) string {
+	if address.Equal(net.IPv4(127, 0, 0, 1)) {
+		return ":8000" //It would be silly to do something funny on your own computer
+	}
+	port := ComputeHash(address)
+	return ":" + strconv.Itoa(port)
+}
 
