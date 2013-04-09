8da635b10ced9cd4554a32dc368d395abd296034
Brendan Higgins <bjh83@case.edu>
2013-04-09 17:25:43 -0400

    I updated the html page to reflect changes

--- htmlgui.go
+++ htmlgui.go
@@ -1,40 +1,36 @@
 package main
 
 import (
-  "html/template"
-  "net/http"
-  "net"
+	"html/template"
+	"net/http"
+	"net"
 )
 
 var templates = template.Must(template.ParseFiles("templates/index.html", "templates/query_result.html"))
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-  templates.ExecuteTemplate(w, "index.html", nil)
+	templates.ExecuteTemplate(w, "index.html", nil)
 }
 
 func searchHandler(w http.ResponseWriter, r *http.Request) {
-  f := Search(r.FormValue("search"))
-  templates.ExecuteTemplate(w, "query_result.html", f)
+	f := Search(r.FormValue("search"))
+	templates.ExecuteTemplate(w, "query_result.html", f)
 }
 
 func downloadHandler(w http.ResponseWriter, r *http.Request) {
-  DownloadQueue <- &IPFilePair{IP: net.ParseIP(r.FormValue("ip")), FileName: r.FormValue("file")}
-}
-
-func updateHandler(w http.ResponseWriter, r *http.Request) {
-  BuildFileManifest()
+	DownloadQueue <- &IPFilePair{IP: net.ParseIP(r.FormValue("ip")), FileName: r.FormValue("file")}
 }
 
 func killHandler(w http.ResponseWriter, r *http.Request) {
-  Shutdown()
+	Shutdown()
 }
 
 func InitializeFancyStuff() {
-  http.HandleFunc("/", homeHandler)
-  http.HandleFunc("/search", searchHandler)
-  http.HandleFunc("/download", downloadHandler)
-  http.HandleFunc("/update", updateHandler)
-  http.HandleFunc("/kill", killHandler)
-  http.Handle("/static/", http.FileServer(http.Dir("./")))
-  http.ListenAndServe(":8000", nil)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/search", searchHandler)
+	http.HandleFunc("/download", downloadHandler)
+	http.HandleFunc("/kill", killHandler)
+	http.Handle("/static/", http.FileServer(http.Dir("./")))
+	http.ListenAndServe(":8000", nil)
 }
+
