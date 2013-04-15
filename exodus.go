4e559a20478e559b23448732777c0a03abf76b65
Brendan Higgins <bjh83@case.edu>
2013-04-15 17:11:03 -0400

    BlackWhiteList is now saved on shutdown

--- exodus.go
+++ exodus.go
@@ -184,6 +184,7 @@ func BootStrap() error {
 
 func Shutdown() {
 	ioutil.WriteFile(ConfPath("peerlist"), AddressList.Contents().Marshal(), os.ModePerm)
+	ioutil.WriteFile(ConfPath("blackwhitelist.xml"), BlackWhiteList.Marshal(), os.ModePerm)
 	err := ioutil.WriteFile(ConfPath("manifest-cache.json"), FileManifest.Marshal(), os.ModePerm)
 	if err != nil {
 		log.Println(err)
