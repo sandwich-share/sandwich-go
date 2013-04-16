d093efc3ee2f1cc1549a96728e8bf6a2619d291c
Cameron Gutman <aicommander@gmail.com>
2013-04-16 15:27:41 -0700

    Update PowerShell script for versioning

--- build.ps1
+++ build.ps1
@@ -1,4 +1,17 @@
-$VERSION = git rev-parse --short HEAD
+$GITVER = git rev-parse --short HEAD
+
+$VERSION = "0.9"
+
+$RELEASE = 0
+if ($args.count -gt 0) {
+	if ($args[0].CompareTo("release") -eq 0) {
+		$RELEASE = 1
+	}
+}
+
+if ($RELEASE -eq 0) {
+	$VERSION += ":"+$GITVER
+}
 
 echo "package main`n`nconst VERSION = `"$VERSION`"`n" | Out-File -Encoding "ascii" VERSION.go
 
