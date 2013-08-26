285f56a639c0418470202b5110fd5a61a9457dec
Cameron Gutman <cameron.gutman@case.edu>
2013-08-26 01:34:31 -0400

    Fix PowerShell build script

--- build.ps1
+++ build.ps1
@@ -3,21 +3,31 @@ $GITVER = git rev-parse --short HEAD
 $VERSION = "1.0"
 
 $RELEASE = 0
+$UPDATE = 0
+
 if ($args.count -gt 0) {
 	if ($args[0].CompareTo("release") -eq 0) {
 		$RELEASE = 1
+	} else if ($args[0].CompareTo("update") -eq 0) {
+		$UPDATE = 1
 	}
 }
 
 if ($RELEASE -eq 0) {
-    echo "Building local version"
+	echo "Building local version"
 	$VERSION += ":"+$GITVER
 } else {
-    echo "Building release version"
+	echo "Building release version"
 }
 
-echo "package main`n`nconst VERSION = `"$VERSION`"`n" | Out-File -Encoding "ascii" VERSION.go
+echo "package util`n`nconst VERSION = `"$VERSION`"`n" | Out-File -Encoding "ascii" util/VERSION.go
 
-go build
+if ($UPDATE -eq 0) {
+	go build
+} else {
+	echo "Updating dependencies"
+	go get
+}
 
-rm VERSION.go
+
+rm util/VERSION.go
